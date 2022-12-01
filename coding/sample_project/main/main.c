9132f5ad29474206ae3593b083a9c601d8de29f5
ancardenas <ancardenas@unal.edu.co>
2022-11-30 21:50:54 -0500

    Modularizacion del codigo
    Se agregaron los modulos, dht, vetilador

--- coding/sample_project/main/main.c
+++ coding/sample_project/main/main.c
@@ -8,16 +8,38 @@
 #include "modulos/ultrasonido.h"
 #include "modulos/blink.h"
 #include "modulos/soil_humidity.h"
+#include "modulos/dht11.h"
 
-
+#include"actuadores/vent.h"
 
 
 
 
 void app_main()
-{
-    xTaskCreate(ultrasonic_test, "ultrasonic_test", configMINIMAL_STACK_SIZE * 3, NULL, 3, NULL);
+{   
+    set_PWM();
+    set_timer();
+
+    /*xTaskCreate(ultrasonic_test, "ultrasonic_test", configMINIMAL_STACK_SIZE * 3, NULL, 3, NULL);
     xTaskCreate(blink,"blink",configMINIMAL_STACK_SIZE * 3, NULL, 4, NULL);
-    xTaskCreate(soil_humidity,"soil_humidity",configMINIMAL_STACK_SIZE*3,NULL,5,NULL);
+    xTaskCreate(soil_humidity,"soil_humidity",configMINIMAL_STACK_SIZE*3,NULL,5,NULL);*/
+    xTaskCreate(dht11,"dht11",configMINIMAL_STACK_SIZE*3,NULL, 5, NULL);
+    while (1)
+    {
+        printf("Temperatura %fC \n",temp);/* code */
+        vTaskDelay(100);
+        if(temp>22){
+            setDuty(900);
+            vTaskDelay(200);
+        }else{
+            setDuty(0);
+            
+        }
+    
+    
+    }
+    
+        printf("Temperatura %.1f%%",temp);
+
 }
 
---
+++ coding/sample_project/main/modulos/dht11.h
@@ -0,0 +1,6 @@
+
+
+float hum;
+float temp;
+
+void dht11(void *pvParameters);
